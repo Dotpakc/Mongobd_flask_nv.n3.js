c948d3650ce365f4be4667d3c09ce37217ec3457
DotPakc <lifirenko123@gmail.com>
2016-11-09 23:31:35 +0200

    deBAG

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ def sort():
     global base
     base=[]
     proof = []
+    i=0
 
-    cities = list(db.us_cont.find().sort('pop', -1)) # import from mongodb 
-    while len(proof)<21:
+    cities = list(db.us_cont.find().sort('pop', -1)) #limit can >50
+    while len(proof) < 21:
         # dont repeat
-        if cut["city"] not in proof:
-                proof.append(cut["city"])
-                base.append({"label":cut["city"],"value":str(cut["pop"])}) 
+        if cities[i]["city"] not in proof:
+            proof.append(cities[i]["city"])
+            base.append({"label": cities[i]["city"], "value": str(cities[i]["pop"])})
+        i += 1
 
 #request main
 @app.route('/')
